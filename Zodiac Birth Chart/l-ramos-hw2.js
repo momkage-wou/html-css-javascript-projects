197bed17e261dffda68d0778f0df8b39b54e3f62
Alexis Ramos <146020112+momkage-wou@users.noreply.github.com>
2024-01-26 21:44:01 -0800

    Add files via upload

---
+++ Zodiac Birth Chart/l-ramos-hw2.js
@@ -0,0 +1,8 @@
+// carousel gallery of zodiac signs that pauses when clicked
+
+// geolocation when user types in city name
+
+// data cannot be null
+
+// when submit is clicked the birth chart is calculated and displayed in a table on screen
+
